55c62d9811342ee8b1e07090f9d7f08dc18363c2
karkirajendra <rajendrakarki0614@gmail.com>
2025-12-15 17:04:01 +0545

    auth creation

---
+++ app/Models/User.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model{
+
+protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'role',
+    ];
+
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    public function blogs()
+    {
+        return $this->hasMany(Blog::class);
+    }
+};
